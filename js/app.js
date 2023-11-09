484d83fbdfcdf423b150d63af6b8d40f63bcffb7
BADHAN SEN <96924254+Badhon9080@users.noreply.github.com>
2023-11-10 00:55:32 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//SJSVBMNLOVE
+
+$('.btn').click(function(){
+    $('.test').toggleClass('active')
+})
+new VenoBox({
+    spinner: 'plane',
+    spinColor: '#266d40',
+    titleattr: 'data-title',
+    toolsColor : '#cfc91d',
+ });
+
+ 
+
+      
